a665822a6f01db0b0d6a5417e68535ff2b77239f
atmorfaw10 <asongtmorfaw_sd10@users.noreply.github.com>
2022-09-03 20:35:09 -0400

    t-002: chore: removed implicit role from button tag

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -10,7 +10,7 @@ function Landing() {
                 <h1>Feel the Music</h1>
                 <h3>Stream over 82 million tracks with one click</h3>
             </div>
-            <button role = 'button' className="landing-buttn">Stream Now</button>
+            <button className="landing-buttn">Stream Now</button>
 
         </>
     );
